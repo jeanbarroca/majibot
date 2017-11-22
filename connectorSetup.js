d1b1c9f6afea7fb49fa252e459992b7e47f14c13
Jean Barroca <jeanbarroca@gmail.com>
2017-11-22 13:18:30 +0000

    Heroku // Fixes to work with Heroku
    Fixed PORT env for heroku
    Added start script for heroku

--- connectorSetup.js
+++ connectorSetup.js
@@ -14,7 +14,7 @@ module.exports = () => {
     // Setup Restify Server
     const server = restify.createServer();
 
-    server.listen(process.env.port || 3978, function () {
+    server.listen(process.env.PORT || 3978, () => {
         console.log('%s listening to %s', server.name, server.url);
     });
     server.post('/api/messages', connector.listen());
