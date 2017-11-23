5f7d3d5bd4b05754ace32ebd00952c733ab2a793
Jean Barroca <jeanbarroca@gmail.com>
2017-11-23 23:14:15 +0000

    Clean up, easier to read structure for problem submission

--- dialogs/submitPhone.js
+++ dialogs/submitPhone.js
@@ -1,10 +1,14 @@
 module.exports = () => {
     bot.dialog('/submitPhone', [
-        (session, next) => {
+        (session) => {
             builder.Prompts.text(session, 'SubmitPhone');
         },
-        (session, results, next) => {
+        (session, results) => {
             session.userData.Phone = results.response;
+
+            /* TODO: Confirm phone number after submission.
+               TODO: Validate phone number */
+
             session.endDialog();
         }
 
--- dialogs/submitProblem.js
+++ dialogs/submitProblem.js
@@ -1,53 +1,81 @@
 module.exports = () => {
-
-    const request = require('request');
-
     bot.dialog('/submitProblem', [
         // Step 1: Choose problem category
         (session, next) => {
+            session.beginDialog('/requestServiceCategory');
+        },
+        // Step 2: Request location
+        (session, results, next) => {
+            session.beginDialog('/requestLocation');
+        },
 
+        // Step 3: Additional details
+        (session, results, next) => {
+            session.beginDialog('/requestAdditionalDetails');
+        }
+    ]);
+
+    bot.dialog('/requestServiceCategory', [
+        (session, next) => {
             getServices(process.env.OPEN311_ENDPOINT + 'services.json', (err, results) => {
                 if (err) {
                     session.error(err);
                 } else {
                     let choices = results.map((service) => service.service_name);
+                    session.dialogData.services = results;
                     builder.Prompts.choice(session, 'SubmitProblem', choices);
                 }
             });
         },
-
-        // Step 2: Request location
-        (session) => {
-            quickReplies.LocationPrompt.beginDialog(session);
-        },
-        (session, args, next) => {
-            if (args.response) {
-                var location = args.response.entity;
-                session.send(`Your location is : ${location.title}, Longitude: ${location.coordinates.long}, Latitude: ${location.coordinates.lat}`);
-            }
-
-            // is user's phone set? if not, request it.
-
-            let phone = session.userData.Phone;
-            if (!phone) {
-                session.replaceDialog('/submitPhone');
-            }
-        },
-        // Step 3: Additional details
-        (session, args, next) => {
-            let options =  session.localizer.gettext(session.preferredLocale(), "AdditionalDetailsOptions");
-            builder.Prompts.choice(session, 'AdditionalDetailsPrompt', options);
+        (session, results, next) => {
+            session.conversationData.service_code = session.dialogData.services[results.response.index].service_code;
+            console.log('Service request: ' + session.conversationData.service_code);
+            session.send(`You submitted ${session.dialogData.services[results.response.index].service_name}`);
+            session.endDialogWithResult({'service_name': results.response.entity});
         }
     ]);
 
-    const getServices = (url, callback) => {
-        request(url, (error, response, body) => {
-            if (!error && response && response.statusCode == 200) {
-                let result = JSON.parse(body);
-                callback(null, result);
-            } else {
-                callback(error, null);
+    bot.dialog('/requestLocation', [
+        // Step 2: Request location
+        (session, results, next) => {
+
+            quickReplies.LocationPrompt.beginDialog(session);
+
+        },
+        (session, args, next) => {
+
+            if (args.response) {
+                let location = args.response.entity;
+
+                session.send(`Your location is : ${location.title}, Longitude: ${location.coordinates.long}, Latitude: ${location.coordinates.lat}`);
+                session.conversationData.lat = args.response.entity.location.coordinates.lat;
+                session.conversationData.long = args.response.entity.location.coordinates.long;
+
+                // is user's phone set? if not, request it.
+                if (!session.userData.Phone) {
+                    session.replaceDialog('/submitPhone');
+                }
+
+                session.replaceDialog('/requestAdditionalDetails');
             }
-        });
-    };
+            else {
+                session.replaceDialog('/requestAdditionalDetails');
+            }
+        }
+    ]);
+
+    bot.dialog('/requestAdditionalDetails', [
+        (session, next) => {
+            console.log('Service request additional details: ' + session.conversationData.service_code);
+            let options =  session.localizer.gettext(session.preferredLocale(), "AdditionalDetailsOptions");
+            builder.Prompts.choice(session, 'AdditionalDetailsPrompt', options);
+        },
+        (session, results) => {
+            session.conversationData.description = results.response.entity;
+            console.log(session.conversationData.description);
+            console.log(session.conversationData.service_code);
+            session.send(`Success!\n Service request description\n Phone: ${session.userData.Phone}\n Service code: ${session.conversationData.service_code}\n Coordinates: ${session.conversationData.lat}, ${session.conversationData.long}\n Description: ${session.conversationData.description}`);
+            session.replaceDialog('/');
+        }
+    ]);
 };
