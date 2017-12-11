060e8f81529d7922ed7f0713a09aff8c27401300
Jean Barroca <jeanbarroca@gmail.com>
2017-12-11 07:09:58 +0300

    submitPhone without validation inside, do that it can be used in different contexts

--- dialogs/submitPhone.js
+++ dialogs/submitPhone.js
@@ -1,15 +1,11 @@
 module.exports = () => {
     bot.dialog('/submitPhone', [
         (session, results, next) => {
-            if (session.userData.Phone === null) {
                 builder.Prompts.text(session, 'SubmitPhone');                
-            }
-            else {
-                session.endDialog();                
-            }
         },
         (session, results, next) => {
             session.userData.Phone = results.response;
+            session.send('ThankYouPhoneStored', session.userData.Phone)
 
             /* TODO: Confirm phone number after submission.
                TODO: Validate phone number */
