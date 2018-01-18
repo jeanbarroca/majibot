1a35c582dd7e7b32c1fa234e97c868a32c83070d
Jean Barroca <jeanbarroca@gmail.com>
2018-01-18 17:28:40 +0300

    V1
    * Initial implementation of submit Service Request feature
    
    Initial file organization: app.js / connectorSetup.js / APIHelpers.js
    
    eslint config
    
    Locale dialog for Swahili & English
    
    Heroku // Fixes to work with Heroku
    
    Fixed PORT env for heroku
    Added start script for heroku
    
    Submit problem dialog, initial commit
    
    .gitignore to ignore WebStorm .idea directory
    
    Fixed prompt string, changed flow to continue to main menu
    
    * Implements submission of service request (#1)
    
    * Implements submitProblem (#3)
    
    * Read .service_request_id from first element in list
    
    * Add default coordinates
    
    * serviceRequest reorganized, JSON creation in a more tidy way
    
    * Bye and help global actions
    
    * Localize help message
    
    * Implements phone and language options
    
    * Asks picture, additional details. Sends through API.
    
    * Fixes for Facebook Bot Menu
    
    endDialog if user skips additional details
    
    Add DialogAction submitProblem
    
    testing Facebook persistent menus
    
    Fixed keyword: AddPhoto not AddPicture
    
    Enhanced dialog
    
    * Allow multiple pictures
    
    * add initial tanzania swahili translation (#4)
    
    * End conversation to clear conversationData after submission of problem
    
    * Make initial prompt locale compatible
    
    * Updated README file

---
+++ app.js
@@ -0,0 +1,65 @@
+require('dotenv-extended').load();
+
+require('./connectorSetup')();
+require('./utils.js')();
+
+
+require('./dialogs/selectLocale')();
+require('./dialogs/submitProblem')();
+require('./dialogs/submitPhone')();
+
+
+/*
+require('./dialogs/checkProblems.js')();
+require('./dialogs/checkMyBill.js')();
+*/
+
+
+// Global actions
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^cancel/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+bot.beginDialogAction('language', '/selectLocale', { matches: /^language/i });
+bot.beginDialogAction('phone', '/submitPhone', { matches: /^phone/i });
+bot.beginDialogAction('submitProblem', '/submitProblem');
+
+// Entry point of the bot
+bot.dialog('/', [
+    (session) => {
+        session.replaceDialog('/promptButtons');
+    }
+]);
+
+bot.dialog('/promptButtons', [
+    (session, args, next) => {
+        let choices = session.localizer.gettext(session.preferredLocale(), 'InitialPromptOptions');   
+        builder.Prompts.choice(session, 'InitialPrompt', choices, {'listStyle': 3});
+    },
+    (session, results, next) => {
+        if (results.response) {
+            let selection = results.response.entity;
+
+            // route to corresponding dialogs
+
+            switch (selection) {
+            case 'Submit a problem':
+                session.replaceDialog('/submitProblem');
+                break;
+            case 'Change phone number':
+                session.reset('/submitPhone');
+                break;
+            case 'Change language':
+                session.reset('/selectLocale');
+                break;
+            default:
+                session.reset('/');
+                break;
+            }
+        }
+    }
+]);
+
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog('HelpMessage');
+    }
+]);
---
+++ utils.js
@@ -0,0 +1,26 @@
+module.exports = () => {
+
+    global.request = require('request');
+
+    global.getServices = (callback) => {
+        request(process.env.OPEN311_ENDPOINT + 'services.json', (error, response, body) => {
+            if (!error && response && response.statusCode == 200) {
+                let result = JSON.parse(body);
+                callback(null, result);
+            } else {
+                callback(error, null);
+            }
+        });
+    };
+
+    global.submitServiceRequest = (serviceRequest, callback) => {
+        request.post(process.env.OPEN311_ENDPOINT + 'requests.json', {'form': serviceRequest}, (error, response, body) => {
+            if (!error && response && response.statusCode == 201) {
+                let result = JSON.parse(body);
+                callback(null, result);
+            } else {
+                callback('Something failed, we are sorry.' + JSON.parse(body).message, null);
+            }
+        });
+    };
+};
